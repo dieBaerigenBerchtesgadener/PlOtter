e37a45bd27481f11c3433f080472bf85ae8859f3
dieBaerigenBerchtesgadener <155662947+dieBaerigenBerchtesgadener@users.noreply.github.com>
2024-03-03 14:30:55 +0100

    Update custom.py

--- custom.py
+++ custom.py
@@ -4,8 +4,8 @@ bg = BrachioGraph(
     pw_down=2460,
     pw_up=1950,
     bounds=(-8.5, 4, 4.5, 12),
-    servo_1_parked_pw=1490,
-    servo_2_parked_pw=1450,
+    #servo_1_parked_pw=1490,
+    #servo_2_parked_pw=1450,
     hysteresis_correction_1=7,
     hysteresis_correction_2=7,
 )

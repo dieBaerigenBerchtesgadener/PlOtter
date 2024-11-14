26439a314ec0a3e94c23dafdd7ccaeb7ccb1ed76
dieBaerigenBerchtesgadener <155662947+dieBaerigenBerchtesgadener@users.noreply.github.com>
2024-11-14 18:37:48 +0100

    Update main.py

--- main.py
+++ main.py
@@ -347,4 +347,4 @@ def shutdown():
         return 'Access denied'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=80)
